5a286cf0a27519925d2579d63ddac1ed6b6b6a37
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-27 08:57:54 -0300

    criando a base do site

---
+++ src/js/quiz.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const pergunta = document.getElementById('pergunta');
+    const resposta = document.getElementById('resposta');
+    const proximaPergunta = document.getElementById("proximo");
+    const mensagem = document.getElementById('message');
+    const containerPerguntas = document.getElementById('container-perguntas');
+    const containerResultado = document.getElementById('container-resultado');
+    const listaResultado = document.getElementById('lista-resultado');
+    const reiniciarBotao = document.getElementById('inicio-btn') })
+
+    const questoes=[
+        
+    ]
